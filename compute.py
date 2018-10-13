a8040dca5c34418cd3a8dbae71a0cddb27f86c72
David Chudzicki <dchudz@gmail.com>
2018-10-13 17:47:24 -0400

    updates to vanguard rebalancing

--- compute.py
+++ compute.py
@@ -1,7 +1,15 @@
 import pandas as pd
-df = pd.read_csv("/Users/davidchudzicki/Downloads/ofxdownload (1).csv")  # delete the bottom table manually
+downloaded_data = pd.read_csv("/Users/davidchudzicki/Downloads/ofxdownload (2).csv")  # delete the bottom table manually
 ticker_to_category = pd.read_csv("ticker_to_category.csv")
-by_cat = pd.merge(df, ticker_to_category).drop(df.columns.difference(["Category", "Total Value"]), 1).groupby("Category").sum()
-by_cat['Percent'] = 100*by_cat/by_cat.sum()
-with_targets = pd.read_csv("/Users/davidchudzicki/vanguard/category_targets.csv").merge(by_cat.reset_index())
-print(with_targets)
+by_cat = pd.merge(downloaded_data, ticker_to_category).drop(downloaded_data.columns.difference(["Category", "Total Value"]), 1).groupby("Category").sum()
+df = pd.read_csv("/Users/davidchudzicki/vanguard/category_targets.csv").merge(by_cat.reset_index())
+
+total = df['Total Value'].sum()
+new_total = total + 36000
+
+df = df.rename(columns={'Total Value': 'CurrentDollars'})
+
+
+df['TargetDollars'] = df.TargetProportion * new_total
+
+df['PurchaseAmount'] = df.TargetDollars - df.CurrentDollars
